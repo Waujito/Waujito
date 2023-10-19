dd2368c95a5b069790b20e5711c163b58e2fbff9
Vadim Vetrov <vetrovvd@gmail.com>
2023-10-19 16:27:25 +0300

    Added sportprogramming template

---
+++ other/sportprog_template.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using lll = __int128;
+using ld = long double;
+using db = double;
+
+// pairs
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+#define mp make_pair
+#define f first
+#define s second
+
+// vectors
+using vi = vector<int>;
+using vl = vector<ll>;
+
+// Uncomment to use LONG type
+// #define LONG
+// #define FORCE_LONG
+
+#ifdef LONG
+const ll MOD = 1e9 + 7;
+const ll INF = INT64_MAX - 1;
+
+#ifdef FORCE_LONG
+#define int ll
+#define double ld
+using db = ld;
+using pi = pl;
+using vi = vl;
+#endif
+#else
+const int MOD = 1e9 + 7;
+const int INF = INT32_MAX;
+#endif
+
+void solve() {}
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  solve();
+}
